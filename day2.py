998397f6c259b9c2518a249b74e0c7c28cbb9849
gxm8595 <84980892+gxm8595@users.noreply.github.com>
2021-07-24 08:11:22 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,74 @@
+# name = "Gaurav "
+# print(len(name))
+# print(name.find("r"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())  #if name is = 2,, then answer would be True
+# print(name.isalpha())  #if there is any space then it will show False
+# print(name.count("a"))
+# print(name.replace("a","e"))
+# print(name*4)
+#
+
+# Type casting = Convert a data type of value to another data type
+# x = 1  #int
+# y = 2.0 #float
+# z = "3"  # string
+# int(y)
+#
+#
+#
+# print(x)
+# print(int(y))
+# print(z)
+#
+
+
+
+
+# Maths Function
+# import math
+# pi = 3.14
+# x=1
+# y=2
+# z=3
+# print(max(x,y,z))
+# print(min(x,y,z))
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,3))
+# print(math.sqrt(pi))
+
+
+
+# String slicing
+# Slicing = create a substracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+#
+# name = "Gaurav Sharma"
+# name_1 = name[0:4]
+# name_2 = name[4:13]
+# name_3 = name[::1]
+# reversed_name = name[::-3]
+# print(name_1)
+# print(name_2)
+# print(name_3)
+# print(reversed_name)
+
+
+# game = "http://google.com"
+# slice_1 = slice(7,-4)
+# print(game[slice_1])
+
+
+
+
+
+
+
+
